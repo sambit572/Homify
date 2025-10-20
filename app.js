8c072b77c0bc79672b75f4db727424eec6353272
sambit578 <sambitkumarbal24@gmail.com>
2025-10-20 08:47:06 +0530

    Create Update and Edit Route

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const app=express();
 const mongoose=require("mongoose");
 const Listing=require("./models/listing");
 const path=require("path");
+const methodOverride=require("method-override");
 
 const MONGO_URL='mongodb://127.0.0.1:27017/homify';
 
@@ -19,6 +20,7 @@ main().then(()=>{
 app.set("view engine","ejs");
 app.set("views",path.join(__dirname,"views"));
 app.use(express.urlencoded({extended:true}));
+app.use(methodOverride("_method"));
 
 app.get("/",(req,res)=>{
     res.send("hii,I am the Root");
@@ -42,10 +44,24 @@ app.get("/listings/:id",async(req,res)=>{
     res.render("listings/show.ejs",{listing}); 
 });
 
+//Create Route
 app.post("/listings",async(req,res)=>{
-    let listing=req.body;
-    console.log(listing);
+    const newListing=new Listing(req.body.listing);
+    await newListing.save();
+    res.redirect("/listings");
 });
+
+//Edit Route
+app.get("/listings/:id/edit",async(req,res)=>{
+    let {id}=req.params;
+    const listing=await Listing.findById(id);
+    res.render("listings/edit.ejs",{listing});
+});
+
+//Update Route
+app.put("/listings/:id",(req,res)=>{
+    
+})
 
 app.listen(8080,()=>{
     console.log("Server is running on port 8080");
