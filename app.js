c51ac9f05bce526a5932a5479b7532e3c5cccdf3
sambit578 <sambitkumarbal24@gmail.com>
2025-10-21 12:19:52 +0530

    Creating Update and Delate Route

--- app.js
+++ app.js
@@ -59,8 +59,18 @@ app.get("/listings/:id/edit",async(req,res)=>{
 });
 
 //Update Route
-app.put("/listings/:id",(req,res)=>{
-    
+app.put("/listings/:id",async(req,res)=>{
+    let {id}=req.params;
+    await Listing.findByIdAndUpdate(id,{...req.body.listing});
+    res.redirect(`/listings/${id}`);
+});
+
+//Delete Route
+app.delete("/listings/:id",async(req,res)=>{
+    let {id}=req.params;
+    let deletedListing=await Listing.findByIdAndDelete(id);
+    console.log(deletedListing);
+    res.redirect("/listings");
 })
 
 app.listen(8080,()=>{
