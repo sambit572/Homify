6c9bd1300511cb93415b82d59aa9854fde48ce49
sambit578 <sambitkumarbal24@gmail.com>
2025-11-18 20:18:27 +0530

    Adding Security Feature

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ const Listing=require("./models/listing");
 const path=require("path");
 const methodOverride=require("method-override");
 const ejsMate = require("ejs-mate");
+const wrapAsync=require("./utils/wrapAsync.js");
+const ExpressError=require("./utils/ExpressError.js");
 
 const MONGO_URL='mongodb://127.0.0.1:27017/homify';
 
@@ -30,10 +32,10 @@ app.get("/",(req,res)=>{
 });
 
 //Index Route
-app.get("/listings",async(req,res)=>{
+app.get("/listings",wrapAsync(async(req,res)=>{
     const allListings=await Listing.find({});
     res.render("listings/index.ejs",{allListings});
-});
+}));
 
 //new route
 app.get("/listings/new",(req,res)=>{
@@ -41,48 +43,47 @@ app.get("/listings/new",(req,res)=>{
 });
 
 //Show Route
-app.get("/listings/:id",async(req,res)=>{
+app.get("/listings/:id",wrapAsync(async(req,res)=>{
     let {id}=req.params;
     const listing=await Listing.findById(id);
     res.render("listings/show.ejs",{listing}); 
-});
+}));
 
 //Create Route
-app.post("/listings",async(req,res,next)=>{
-    try{
+app.post("/listings",wrapAsync(async(req,res,next)=>{
         const newListing=new Listing(req.body.listing);
         await newListing.save();
         res.redirect("/listings");
-    }catch(err){
-        next(err);
-    }
-});
+}));
 
 //Edit Route
-app.get("/listings/:id/edit",async(req,res)=>{
+app.get("/listings/:id/edit",wrapAsync(async(req,res)=>{
     let {id}=req.params;
     const listing=await Listing.findById(id);
     res.render("listings/edit.ejs",{listing});
-});
+}));
 
 //Update Route
-app.put("/listings/:id",async(req,res)=>{
+app.put("/listings/:id",wrapAsync(async(req,res)=>{
     let {id}=req.params;
     await Listing.findByIdAndUpdate(id,{...req.body.listing});
     res.redirect(`/listings/${id}`);
-});
+}));
 
 //Delete Route
-app.delete("/listings/:id",async(req,res)=>{
+app.delete("/listings/:id",wrapAsync(async(req,res)=>{
     let {id}=req.params;
     let deletedListing=await Listing.findByIdAndDelete(id);
     console.log(deletedListing);
     res.redirect("/listings");
-}); 
+})); 
 
 app.use((err,req,res,next)=>{
-    res.send("something went wrong");
-})
+    let {statusCode=500,message="Something Went Wrong"}=err;
+    res.status(statusCode).send(message);
+});
+
+
 
 app.listen(8080,()=>{
     console.log("Server is running on port 8080");
