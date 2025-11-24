20f00bcf148b4fa9225f99dace656900987cab0b
sambit578 <sambitkumarbal24@gmail.com>
2025-11-24 20:19:27 +0530

    Creating Review Form

--- app.js
+++ app.js
@@ -7,7 +7,9 @@ const methodOverride=require("method-override");
 const ejsMate = require("ejs-mate");
 const wrapAsync=require("./utils/wrapAsync.js");
 const ExpressError=require("./utils/ExpressError.js");
-const { listingSchema } = require("./schema.js");
+const { listingSchema,reviewSchema } = require("./schema.js");
+const Review=require("./models/review");
+
 
 const MONGO_URL='mongodb://127.0.0.1:27017/homify';
 
@@ -42,6 +44,16 @@ const validateListing=(req,res,next)=>{
     }
 };
 
+const validateReview=(req,res,next)=>{
+    let {error}=reviewSchema.validate(req.body); 
+    if(error){   
+        let errMsg=error.details.map((el)=>el.message).join(",");
+        throw new ExpressError(400, errMsg);
+    } else {
+        next();   
+    }
+};
+
 //Index Route
 app.get("/listings",wrapAsync(async(req,res)=>{
     const allListings=await Listing.find({});
@@ -56,7 +68,7 @@ app.get("/listings/new",(req,res)=>{
 //Show Route
 app.get("/listings/:id",wrapAsync(async(req,res)=>{
     let {id}=req.params;
-    const listing=await Listing.findById(id);
+    const listing=await Listing.findById(id).populate("reviews");
     res.render("listings/show.ejs",{listing}); 
 }));
 
@@ -88,6 +100,17 @@ app.delete("/listings/:id",wrapAsync(async(req,res)=>{
     console.log(deletedListing);
     res.redirect("/listings");
 })); 
+
+//Review Route
+app.post("/listings/:id/reviews",validateReview,wrapAsync(async(req,res)=>{
+    let listing=await Listing.findById(req.params.id);
+    let newReview=new Review(req.body.review);
+    listing.reviews.push(newReview);
+    await newReview.save();
+    await listing.save();
+    console.log("New Review Added");
+    res.redirect(`/listings/${listing._id}`);
+}));
 
 app.all(/.*/,(req,res,next)=>{
     next(new ExpressError(404, "Page Not Found!"));
