c5ef17db1f03d91bcbea4f2ecb63ba32f6bb54c0
sambit578 <sambitkumarbal24@gmail.com>
2025-11-27 20:16:10 +0530

    Creating Middleware for Reviews

--- app.js
+++ app.js
@@ -112,6 +112,15 @@ app.post("/listings/:id/reviews",validateReview,wrapAsync(async(req,res)=>{
     res.redirect(`/listings/${listing._id}`);
 }));
 
+//Delete Review Route
+app.delete("/listings/:id/reviews/:reviewId",wrapAsync(async(req,res)=>{
+    let {id,reviewId}=req.params;
+    await Listing.findByIdAndUpdate(id,{$pull:{reviews:reviewId}});
+    await Review.findByIdAndDelete(reviewId);
+    console.log("Review Deleted");
+    res.redirect(`/listings/${id}`);
+}));
+
 app.all(/.*/,(req,res,next)=>{
     next(new ExpressError(404, "Page Not Found!"));
 }); 
