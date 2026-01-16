3bb31ab9474c8ad040b2615420acfb77751ad064
sambit578 <sambitkumarbal24@gmail.com>
2026-01-16 20:20:15 +0530

    More function

--- routes/user.js
+++ routes/user.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const User=require("../models/user.js");
 const wrapAsync = require('../utils/wrapAsync.js');
 const passport=require("passport");
+const { saveRedirectUrl } = require('../middleware.js');
 
 router.get("/signup", (req, res) => {
     res.render("users/signup.ejs");
@@ -31,8 +32,10 @@ router.get("/login", (req, res) => {
     res.render("users/login.ejs");
 });
 
-router.post("/login", passport.authenticate("local", {failureRedirect:'/login', failureFlash: true}), async(req,res) => {
+router.post("/login",saveRedirectUrl, passport.authenticate("local", {failureRedirect:'/login', failureFlash: true}), async(req,res) => {
   res.flash("success","Logged In Successfully");
+  let redirectUrl = res.locals.redirectUrl|| '/listings';
+  req.redirect(redirectUrl);
 });
 
 router.get("/logout",(req,res,next)=>{
